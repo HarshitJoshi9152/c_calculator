f83169b98193c4dc9b8950cf484907423bddcbf1
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2021-11-07 19:25:03 +0530

    Fixed bad switch statement & Refactored some codeTab
    Tab and Backspace are working now !

--- src/test.c
+++ src/test.c
@@ -23,6 +23,7 @@ Right: \u001b[{n}C
 Left: \u001b[{n}D
 */
 
+typedef enum {false, true} bool;
 typedef enum directions {up, down, right, left} DIRECTION;
 // http://www.robelle.com/library/smugbook/ascii.html
 
@@ -76,28 +77,30 @@ void move(int steps, DIRECTION dir) {
     printf("\x1b[%d%c", steps, dirchar);
 }
 
-void print_until(int *intList, int l) {
+void print_array_until(int *intList, int l) {
     for (int i = 0; i < l; i++) {
         printf("%d ", intList[i]);
     }
     printf("\n");
 }
 
+static int charbuff[LIMIT] = {};
+
+bool buffer_matches(const int *sequence, int seq_len, bool *indentifies) {
+    // change buffer to be a global variable
+    // is it bad to multiply $seq_len with sizeof(int) implicitly ?????
+    // what if we change the type of the array items to soemthing else
+    // like a `short int` or `long long` ;
+    *indentifies = (!memcmp(charbuff, sequence, sizeof(int) * seq_len));
+    return *indentifies;
+}
+
 // lets make it such that if it identifies the input and returns successfully
 // it must always be a control character ! or any other char that need interpretation
 int identify_input(char inputChar, char* str, int *is_control_char) {
+    // printf("<%d>", (int)inputChar);
+
     //use time to clear the buffer
-    // all multi chars input start with \x1b ? 27, 79, 77 -> shift + <enter>
-    // all multi chars input start with \x1b ? 27, 91, 65 -> up arrow
-    // support up, down, left, right arrows !!!
-    /*  ctrl + right arrow
-        Key is 27
-        Key is 91
-        Key is 49
-        Key is 59
-        Key is 53
-        Key is 67
-        */
     //static time_t seconds;
     //time(&seconds);
     //printf("<%ld seconds>", seconds);
@@ -105,109 +108,100 @@ int identify_input(char inputChar, char* str, int *is_control_char) {
     // tab is 9
 
 
-    static int charbuff[LIMIT] = {};
     static int c = 0;
 
     // code to check time interval and clear cache if interval is more than 1 seconds.
     // TODO: iknow make it smaller
-    static struct timeval cache, thistime;
-    gettimeofday(&thistime, NULL); // is seconds 
-    if (thistime.tv_sec - cache.tv_sec >= 1) {
-        //printf("CLEARING CACHE"); //IS THIS CODE REALLY REQUIRED TODO
-        // print_until(charbuff, c);
-        memset(charbuff, 0, sizeof(int) * c);
-        c = 0;
-    }
-    cache.tv_sec = thistime.tv_sec;
+    // static struct timeval cache, thistime;
+    // gettimeofday(&thistime, NULL); // is seconds 
+    // if (thistime.tv_sec - cache.tv_sec >= 1) {
+    //     //printf("CLEARING CACHE"); //IS THIS CODE REALLY REQUIRED TODO
+    //     // print_array_until(charbuff, c);
+    //     memset(charbuff, 0, sizeof(int) * c);
+    //     c = 0;
+    // }
+    // cache.tv_sec = thistime.tv_sec;
 
     static int recording = 0;
-    // finding out which char has been inputed // for control characters
+
     if (inputChar == 27 && !recording) {
         // start recording on ESCAPE (coz thats when escape sequences start
         // stop when a sequence has been completed or too much time has passed idk ? maybe 2 secs
         // add them to a complete history 
-        //charbuff[c++] = (int)inputChar;
         recording = 1;
     }
 
-    if (recording) {
-        charbuff[c++] = (int)inputChar;
+    if (recording) charbuff[c++] = (int)inputChar;
+
+    // character code sequences for control Keys !
+    // TODO i guess they use a lot of stack space.
+    const int up_[]  = {27, 91, 65};
+    const int down_[]  = {27, 91, 66};
+    const int right_[]  = {27, 91, 67};
+    const int left_[]  = {27, 91, 68};
+    const int up_control_pressed[] = {27, 91, 49, 59, 53, 65}; // 5 chars ^[[1;5A
+    const int down_control_pressed[] = {27, 91, 49, 59, 53, 66}; // 5 chars ^[[1;5A
+    const int right_control_pressed[] = {27, 91, 49, 59, 53, 67}; // 5 chars ^[[1;5A
+    const int left_control_pressed[] = {27, 91, 49, 59, 53, 68}; // 5 chars ^[[1;5A
+
+    // const int escape[] = {27, 91, 51, 126};
+
+    // const int back_space =    127;
+    // const int space =         32;
+    // const int tab =           9;
+    // const int enter1 =        13;
+    // const int enter2 =        10;
+
+
+    // for inputs that dont occupy more than one character.  | SWITCH STATEMENTS ARE ESSENTIALLY `GOTO`
+    switch((int)inputChar) {
+        case 13:    {strcpy(str, "ENTER");break;}
+        case 10:    {strcpy(str, "ENTER");break;}
+        case 127:   {strcpy(str, "BACKSPACE");break;}
+        case 9:     {strcpy(str, "TAB");break;}
+        // for sequences of characters !
+        default: {
+            bool identified = false;
+            if (buffer_matches(up_, 3, &identified)) {
+                strcpy(str, "UP_ARROW");
+            }
+            else if (buffer_matches(down_, 3, &identified)) {
+                strcpy(str, "DOWN_ARROW");
+            }
+            else if (buffer_matches(right_, 3, &identified)) {
+                strcpy(str, "RIGHT_ARROW");
+            }
+            else if (buffer_matches(left_, 3, &identified)) {
+                strcpy(str, "LEFT_ARROW");
+            }
+            // Control Pressed Arrow Keys !
+            else if (buffer_matches(up_control_pressed, 6, &identified)) {
+                strcpy(str, "UP_ARROW_CTRL");
+            }
+            else if (buffer_matches(down_control_pressed, 6, &identified)) {
+                strcpy(str, "DOWN_ARROW_CTRL");
+            }
+            else if (buffer_matches(right_control_pressed, 6, &identified)) {
+                strcpy(str, "RIGHT_ARROW_CTRL");
+            }
+            if (buffer_matches(left_control_pressed, 6, &identified)) {
+                strcpy(str, "LEFT_ARROW_CTRL");
+            }
+
+            if (identified)
+            {
+                memset(charbuff, 0, sizeof(int)* (c + 1)); // remember c is an index and indexes start at zero
+                c = 0;
+                recording = 0;
+                // *is_control_char = 1;
+            }
+        }
     }
 
-    int up_[]  = {27, 91, 65};
-    int down_[]  = {27, 91, 66};
-    int right_[]  = {27, 91, 67};
-    int left_[]  = {27, 91, 68};
-    int up_control_pressed[] = {27, 91, 49, 59, 53, 65}; // 5 chars ^[[1;5A
-    int down_control_pressed[] = {27, 91, 49, 59, 53, 66}; // 5 chars ^[[1;5A
-    int right_control_pressed[] = {27, 91, 49, 59, 53, 67}; // 5 chars ^[[1;5A
-    int left_control_pressed[] = {27, 91, 49, 59, 53, 68}; // 5 chars ^[[1;5A
-
-    // for inputs that dont occupy more than one character.
-    switch(inputChar) {
-        case 13:// ^M
-            strcpy(str, "enter");
-        default:
-            {
-                //ReverseArray(charbuff, c);
-                if (memcmp(charbuff, up_, sizeof(int) * 3) == 0) {
-                    memset(charbuff, 0, sizeof(int)*c);
-                    c = 0;
-                    strcpy(str, "UP_ARROW");
-                    recording = 0;
-                    *is_control_char = 1;
-                }
-                if (memcmp(charbuff, down_, sizeof(int) * 3) == 0) {
-                    memset(charbuff, 0, sizeof(int)*c);
-                    c = 0;
-                    strcpy(str, "DOWN_ARROW");
-                    recording = 0;
-                    *is_control_char = 1;
-                }
-                if (memcmp(charbuff, right_, sizeof(int) * 3) == 0) {
-                    memset(charbuff, 0, sizeof(int)*c);
-                    c = 0;
-                    strcpy(str, "RIGHT_ARROW");
-                    recording = 0;
-                    *is_control_char = 1;
-                }
-                if (memcmp(charbuff, left_, sizeof(int) * 3) == 0) {
-                    memset(charbuff, 0, sizeof(int)*c);
-                    c = 0;
-                    strcpy(str, "LEFT_ARROW");
-                    recording = 0;
-                    *is_control_char = 1;
-                }
-                // Control Pressed Arrow Keys !
-                if (memcmp(charbuff, up_control_pressed, sizeof(int) * 6) == 0) {
-                    memset(charbuff, 0, sizeof(int)*c);
-                    c = 0;
-                    strcpy(str, "up_arrow_ctrl");
-                    recording = 0;
-                    *is_control_char = 1;
-                }
-                if (memcmp(charbuff, down_control_pressed, sizeof(int) * 6) == 0) {
-                    memset(charbuff, 0, sizeof(int)*c);
-                    c = 0;
-                    strcpy(str, "down_arrow_ctrl");
-                    recording = 0;
-                    *is_control_char = 1;
-                }
-                if (memcmp(charbuff, right_control_pressed, sizeof(int) * 6) == 0) {
-                    memset(charbuff, 0, sizeof(int)*c);
-                    c = 0;
-                    strcpy(str, "right_arrow_ctrl");
-                    recording = 0;
-                    *is_control_char = 1;
-                }
-                if (memcmp(charbuff, left_control_pressed, sizeof(int) * 6) == 0) {
-                    memset(charbuff, 0, sizeof(int)*c);
-                    c = 0;
-                    strcpy(str, "left_arrow_ctrl");
-                    recording = 0;
-                    *is_control_char = 1;
-                }
-            }
+    if (!strcmp(str, "")) {
+        *is_control_char = 0;
+    } else {
+        *is_control_char = 1;
     }
 
     if (!recording) { 
@@ -249,15 +243,22 @@ int main(void)
             // backspace, arrow keys etc ! (enter is not a control chracter but still needs to be cleared  how about we just clear it all and rewrite anyways ?)
             // the input is a control character ! that we need to react to.
             // clear the length of chars written by the control character
-            if (!strncmp(str, "enter", 5)) {
+
+            printf("<%s>",str);
+            if (!strncmp(str, "TAB", 4)) {
+                buffer[c++] = '\t';
+            }
+            else if (!strncmp(str, "BACKSPACE", 10)) {
+                buffer[--c] = '\000';
+            }
+            // ENTER DOESNT WORK !
+            else if (!strncmp(str, "ENTER", 6)) {
                 // evaluate the result and move to a newline for further evaluation
-                printf("\n");
-                printf("\x1b[1000d");
+                printf("\n"); // i dont think this will move us to the next line !
+                printf("\x1b[1000D");
+                strcpy(buffer, ""); // or memset maybe
                 continue;
             }
-            //else if (str == "backspace") {
-
-            //}
         }
         else {
             // add character to buffer !
@@ -266,7 +267,6 @@ int main(void)
         // now all the printed control characters get cleared because we rerender the buffer
         // on each input character !
         render(buffer);
-        printf("%s", str);
         //move(1, up);
     }    
     // set cursor position to left most point on newline when exiting 
